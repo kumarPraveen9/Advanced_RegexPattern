00d70093524ca8df8517e4cc82aa1d18edb7cadb
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-16 19:59:52 +0530

    validating the given mail id using regex

--- Valid_mail.cs
+++ Valid_mail.cs
@@ -7,20 +7,24 @@ namespace day16
     {
         public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)*@(bl).(co)([.][a-z]{2,})?$";
         public static void Main(String[] args)
         {
-            Console.WriteLine("enter your First name name");
+            Console.WriteLine("enter your First  name");
             string name = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("enter your Last name name");
+            Console.WriteLine("enter your Last  name");
             string lname = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("enter your mail");
+            string mail = Console.ReadLine();
             Console.WriteLine();
 
 
 
             bool val1 = Regex.IsMatch(name, Reg_pattern1);
             bool val2 = Regex.IsMatch(lname, Reg_pattern2);
-
+            bool val3 = Regex.IsMatch(mail, Reg_pattern3);
 
 
 
@@ -29,13 +33,19 @@ namespace day16
                 Console.WriteLine("Pattern is correct for given First name: {0}",name);
             }
             else
-                Console.WriteLine("pattern is wrong");
+                Console.WriteLine("pattern First name is wrong");
             if (val2)
             {
                 Console.WriteLine("Pattern is correct for given Last name: {0}", lname);
             }
             else
-                Console.WriteLine("pattern is wrong");
+                Console.WriteLine("pattern Last name is wrong");
+            if (val3)
+            {
+                Console.WriteLine("Pattern is correct for given mail ID : {0}", mail);
+            }
+            else
+                Console.WriteLine("pattern mail ID  is wrong");
 
         }
     }
