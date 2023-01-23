c325c2292e6dba9a7935b55115d44607e3421332
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-23 19:23:34 +0530

    test cases for all using try&catch

--- TestCases.cs
+++ TestCases.cs
@@ -8,93 +8,173 @@ namespace RegexTestCase
     [TestClass]
     public class UnitTest1
     {
+        /* try
+            {
+                Result = validation.Test_First(firstName);
+                Assert.AreEqual("First Name is valid", Result);
+            }
+            catch (CustomExceptions ex)
+            {
+                Assert.AreEqual("\nMinimum length three characters \nFirst letter should be " +
+                    "capital\n", ex.Message);
+            }*/
         [TestMethod]
         public void UseCase1()
         {
-            string name = "Shankerma";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validname(name);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string name = "Shankerma";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validname(name);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid first name", ex.Message);
+            }
         }
 
         [TestMethod]
         public void UseCase2()
         {
-            string lastname = "ShivajiMa";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validlname(lastname);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string lastname = "ShivajiMa";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validlname(lastname);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid last name", ex.Message);
+            }
         }
 
         [TestMethod]
         public void UseCase3()
         {
-            string email = "abc.xyz@bl.co.in";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validmail(email);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string[] emails = new string[3] { "abc.xyz@bl.co.in", "a123@bl.co.in", "fhjjm33@bl.co" };
+
+                for (int i = 0; i < 3; i++)
+                {
+
+                    bool expected = true;
+                    demo program = new demo();
+                    bool actual = program.validmail(emails[i]);
+                    Assert.AreEqual(expected, actual);
+                }
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid mail", ex.Message);
+            }
+
         }
 
         [TestMethod]
         public void UseCase4()
         {
-            string mb = "91 7330827008";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validmobile(mb);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string mb = "91 7330827008";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validmobile(mb);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid mobile number", ex.Message);
+            }
         }
         [TestMethod]
         public void UseCase5()
         {
-            string password = "Ooliisaap";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validpwd1(password);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string password = "Ooliisaap";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validpwd1(password);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid password", ex.Message);
+            }
+
         }
 
         [TestMethod]
         public void UseCase6()
         {
-            string password = "Praveenn";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validpwd2(password);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string password = "Praveenn";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validpwd2(password);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid password", ex.Message);
+            }
         }
 
         [TestMethod]
         public void UseCase7()
         {
-            string password = "Praveen9";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validpwd3(password);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string password = "Praveen9";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validpwd3(password);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid password", ex.Message);
+            }
         }
 
         [TestMethod]
         public void UseCase8()
         {
-            string password = "Pravee$9";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validpwd(password);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string password = "Pravee$9";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validpwd(password);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid password", ex.Message);
+            }
         }
 
         [TestMethod]
         public void UseCase9()
         {
-            string email1 = "abc.xyz@gmail.com";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validmail(email1);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                string email1 = "abc.xyz@gmail.com";
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validmail(email1);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex)
+            {
+                Assert.AreEqual("invalid password", ex.Message);
+            }
         }
     }
 }
